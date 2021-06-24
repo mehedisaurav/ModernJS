53ee2e24dbd03f1bbfa82fbe2b927f54a0ec9ed7
Mehedi <mehedi@techintel.com.bd>
2021-06-24 19:32:39 +0600

    commit

--- function_prototype.js
+++ function_prototype.js
@@ -3,9 +3,9 @@ const flexBoxes = document.querySelectorAll("div .flexbox-item")
 flexBoxes.forEach((box)=>{
     box.addEventListener("click", ()=>{
         // console.log( document.get(box.className.split(' ')[1]))
-        document.querySelector(`.${box.className.split(' ')[1]}`).style.backgroundColor='white';
         var sound = new Audio("click_sound.mp3");
-        sound.play();
-        sound.pause();
+        sound.play();        
+        document.querySelector(`.${box.className.split(' ')[1]}`).style.backgroundColor='white';
+        // sound.pause();
     })
 })
