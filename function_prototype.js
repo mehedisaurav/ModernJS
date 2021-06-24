f95f00b83e097e356593bf208648c5cd2820955d
Mehedi <mehedi@techintel.com.bd>
2021-06-24 19:24:38 +0600

    audio

--- function_prototype.js
+++ function_prototype.js
@@ -4,5 +4,8 @@ flexBoxes.forEach((box)=>{
     box.addEventListener("click", ()=>{
         // console.log( document.get(box.className.split(' ')[1]))
         document.querySelector(`.${box.className.split(' ')[1]}`).style.backgroundColor='white';
+        var sound = new Audio("click_sound.mp3");
+        sound.play();
+        sound.pause();
     })
 })
